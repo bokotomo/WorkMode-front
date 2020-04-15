e017169ef961d5006891aab25589294264714a2c
bokotomo <bokotomo@me.com>
2020-04-15 13:16:34 +0900

    feat

--- src/controller/authentication.ts
+++ src/controller/authentication.ts
@@ -4,16 +4,16 @@ import { ActionModal } from '../redux/actions/modal';
 
 export const requestAuthentication = (socket: WebSocket, token: string) => {
     const action = 'sendmessage'
-    const type = 'authentication'
-    const data = JSON.stringify({
-        type,
+    const role = 'authentication'
+    const data = {
+        role,
         token
-    })
+    }
     socket.send(JSON.stringify({ action, data }));
 };
 
 export const responseAuthentication = (message: MessageEvent, socket: WebSocket, dispatch: Dispatch) => {
-    console.log(message);
+    console.log(message.data);
     alert("KO");
     dispatch(ActionModal.updateModalOpened('register'))
     // authentication token
