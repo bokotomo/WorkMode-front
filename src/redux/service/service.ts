026e6e1a426d0559c22ecdbe27ac8ef10d684eec
bokotomo <bokotomo@me.com>
2020-05-13 09:57:23 +0900

    feat

--- src/redux/service/service.ts
+++ src/redux/service/service.ts
@@ -1,7 +1,11 @@
 import { Dispatch } from 'redux';
 import { TaskCard } from '@/types/taskBoard';
 import { modalOpend } from '@/redux/service/modal';
-import { userRegister } from '@/redux/service/user';
+import {
+  userRegister,
+  userRegisterGuest,
+  userSignin,
+} from '@/redux/service/user';
 import { setWebsocket } from '@/redux/service/socket';
 import {
   addTask,
@@ -13,12 +17,21 @@ import {
 export const service = {
   modalOpend: (dispatch: Dispatch, openedModalName: string) =>
     modalOpend(dispatch, openedModalName),
-  userRegister: (dispatch: Dispatch, socket: WebSocket, name: string) =>
-    userRegister(dispatch, socket, name),
+  userRegister: (
+    dispatch: Dispatch,
+    socket: WebSocket,
+    name: string,
+    email: string,
+    password: string
+  ) => userRegister(dispatch, socket, name, email, password),
   userRegisterGuest: (dispatch: Dispatch, socket: WebSocket, name: string) =>
     userRegisterGuest(dispatch, socket, name),
-  userSignin: (dispatch: Dispatch, socket: WebSocket, name: string) =>
-    userSignin(dispatch, socket, name),
+  userSignin: (
+    dispatch: Dispatch,
+    socket: WebSocket,
+    email: string,
+    password: string
+  ) => userSignin(dispatch, socket, email, password),
   setWebsocket: (dispatch: Dispatch) => setWebsocket(dispatch),
   addTask: (dispatch: Dispatch, socket: WebSocket, task: TaskCard) =>
     addTask(dispatch, socket, task),
--- src/redux/service/user.ts
+++ src/redux/service/user.ts
@@ -1,10 +1,30 @@
 import { Dispatch } from 'redux';
 import { requestUserCreate } from '@/websocket/request/user';
 
+// ユーザ新規登録
 export const userRegister = (
+  dispatch: Dispatch,
+  socket: WebSocket,
+  name: string,
+  email: string,
+  password: string
+) => {
+  requestUserCreate(socket, name, email, password);
+};
+
+// ユーザ新規ゲスト登録
+export const userRegisterGuest = (
   dispatch: Dispatch,
   socket: WebSocket,
   name: string
 ) => {
-  requestUserCreate(socket, name);
+  requestUserGuestCreate(socket, name);
 };
+
+// サインイン
+export const userSignin = (
+  dispatch: Dispatch,
+  socket: WebSocket,
+  email: string,
+  password: string
+) => {};
